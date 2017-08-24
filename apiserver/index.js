b310bfa02423a8c5070876b11b23e7646442ea52
Darryl Kilzer <darrylkilzer@gmail.com>
2017-08-24 07:21:55 -0600

    adding auth for server

--- apiserver/index.js
+++ apiserver/index.js
@@ -2,15 +2,24 @@ var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var cors = require('cors')
-//connect mlab at some point when ready
-// var dbConnect = require('.config/db/mlab-config')
+var sessions = require('sessions')
+var dbConnect = require('.config/db/mlab-config')
 var port = 3000
 
 var server = express()
 
-server.use(cors())
+server.use('/', cors({
+    origin: 'http://localhost:8080',
+    credentials: true
+}))
+
+server.use(sessions)
+server.use(express.static(__dirname + '/www'))
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
+
+var authRouter = require("./routes/auth")
+server.use('/', authRouter)
 
 server.listen(port, () => {
   console.log('Server is listening on port: ', port)
