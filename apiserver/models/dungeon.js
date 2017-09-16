79a053c43a776f3c03a6239109287acf60363b1e
Darryl Kilzer <darrylkilzer@gmail.com>
2017-09-16 10:25:32 -0600

    dungeon schema created

---
+++ apiserver/models/dungeon.js
@@ -0,0 +1,17 @@
+var models = require('../config/constants').models
+let mongoose = require('mongoose')
+let ObjectId = mongoose.Schema.ObjectId
+
+var schema = new mongoose.Schema({
+	name: { type: String, required: true },
+  description: { type: String },
+  rooms: {type: Object, required: true, default: {}},
+  cards: {type: ObjectId, ref: models.card.name, required: true},
+  created: { type: Number, default: Date.now() },
+  private: {type: Boolean, default: true, required: true},
+  creatorId: { type: ObjectId, ref: models.user.name, required: true }
+});
+
+
+
+module.exports = mongoose.model(models.deck.name, schema);
